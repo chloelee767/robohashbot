c9301bba24c5799751142a0ebd75fa2a704fd840
chloelee767 <echloelee@gmail.com>
2020-10-19 13:44:27 +0800

    Add telgram bot API

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/chloelee767/robohashbot
 
 go 1.15
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	gopkg.in/telegram-bot-api.v4 v4.6.4
+)
--- main.go
+++ main.go
@@ -3,33 +3,35 @@ package main
 import (
 	"fmt"
 	"github.com/chloelee767/robohashbot/robohash"
+
+	tgbotapi "gopkg.in/telegram-bot-api.v4"
 )
 
 
 func main() {
-	r1, err1:= robohash.NewRobohash("bob", robohash.Robot)
-	r2, err2 := robohash.NewRobohash("meow!!", robohash.Cat)
-	r4, err4 := robohash.NewRobohash("", robohash.Cat)
-
-	// we can now check whether we have created a valid Robohash
-	if err1 == nil {
-		fmt.Println(r1, r1.GetUrl())
+	bot, botCreationErr := tgbotapi.NewBotAPI("1192566547:AAEQ_FH2mCvL2spwloOnAaMneaa7_ya_3f8")
+	if botCreationErr != nil {
+		fmt.Println(botCreationErr)
+		return
 	}
 
-	if err2 == nil {
-		fmt.Println(r2, r2.GetUrl())
+	updates, updateChanErr := bot.GetUpdatesChan(tgbotapi.UpdateConfig{Timeout: 60})
+	if updateChanErr != nil {
+		fmt.Println(updateChanErr)
+		return
 	}
 
-	if err4 == nil {
-		fmt.Println(r4, r4.GetUrl())
-	} else {
-		fmt.Println("err4:", err4)
+	fmt.Println("Listening...")
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		message := update.Message
+		chatID := message.Chat.ID
+		r, _ := robohash.NewRobohash("meow!!", robohash.Cat)
+		response := tgbotapi.NewPhotoShare(chatID, r.GetUrl())
+		bot.Send(response)
 	}
-
-	// All of this will not compile now as the struct fields are unexported:
-	// r1 := robohash.Robohash{"bob", robohash.Robot}
-	// r2 := robohash.Robohash{"meow!!", robohash.Cat}
-	// r3 := robohash.Robohash{"how about monkeys", roboash.Type{"monkey", 6}}
-	// robohash.Cat.setNumber = 200
-
 }
